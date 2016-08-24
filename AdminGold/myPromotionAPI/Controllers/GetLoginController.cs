16e5c80a240393f578bcdf3a88e1324b5b5729d4
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-08-24 14:33:00 +0700

    no message

---
+++ AdminGold/myPromotionAPI/Controllers/GetLoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using myPromotionAPI.Models;
+
+namespace myPromotionAPI.Controllers
+{
+    public class GetLoginController : ApiController
+    {
+        MyPromotionEntities db = new MyPromotionEntities();
+        // GET: api/GetLogin
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GetLogin/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GetLogin
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/GetLogin/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/GetLogin/5
+        public void Delete(int id)
+        {
+        }
+        public IList<UserDto> GetLogin([FromUri]string username, [FromUri]string password)
+        {
+            object[] para =
+            {
+               new SqlParameter("@userName",username),
+               new SqlParameter("@passWord",password)
+            };
+            var datalogin = db.Database.SqlQuery<UserDto>("exec  sp_login_promotion @userName,@passWord", para);
+            return datalogin.ToList();
+        }
+    }
+}
