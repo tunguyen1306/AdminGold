637f22efecb30627acaae6865fb2facf257ab627
Thien <ducthienweakt@gmail.com>
2017-02-28 13:26:38 +0700

    no message

---
+++ AdminGold/AdminDaviBus/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminDaviBus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FlotCharts()
+        {
+            return View("FlotCharts");
+        }
+
+        public ActionResult MorrisCharts()
+        {
+            return View("MorrisCharts");
+        }
+
+        public ActionResult Tables()
+        {
+            return View("Tables");
+        }
+
+        public ActionResult Forms()
+        {
+            return View("Forms");
+        }
+
+        public ActionResult Panels()
+        {
+            return View("Panels");
+        }
+
+        public ActionResult Buttons()
+        {
+            return View("Buttons");
+        }
+
+        public ActionResult Notifications()
+        {
+            return View("Notifications");
+        }
+
+        public ActionResult Typography()
+        {
+            return View("Typography");
+        }
+
+        public ActionResult Icons()
+        {
+            return View("Icons");
+        }
+
+        public ActionResult Grid()
+        {
+            return View("Grid");
+        }
+
+        public ActionResult Blank()
+        {
+            return View("Blank");
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            if (email=="trafashion@gmail.com" && password=="admin1234")
+            {   Session["user"] = "trafashion@gmail.com";
+               
+
+            }
+            return RedirectToAction("Index", "Home"); ;
+        }
+
+    }
+}
